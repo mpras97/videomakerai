3bd763b34f85ffaafb4b1ae823e98082b8591de3
shivinquartic <shivin@quartic.ai>
2020-08-15 22:20:41 +0530

    ffmpeg addition
    Signed-off-by: shivinquartic <shivin@quartic.ai>

--- core/serializers.py
+++ core/serializers.py
@@ -7,7 +7,8 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('username',)
+        fields = ('id',
+                  'username',)
 
 
 class UserSerializerWithToken(serializers.ModelSerializer):
